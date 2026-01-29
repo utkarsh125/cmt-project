bd30b45bb2ba8c7d6472ee2ab5a5364b6d931377
Utkarsh Pandey <utk2665@gmail.com>
2026-01-29 15:00:27 +0530

    folder divison

---
+++ api/auth.ts
@@ -0,0 +1,16 @@
+//this file will contain all the logic related to
+//authentication (signup)
+//authentication (login)
+
+//
+//fields for signup
+//username
+//email
+//password
+
+//fields for signin
+//email
+//password
+
+//feat: hashing can be used.
+
